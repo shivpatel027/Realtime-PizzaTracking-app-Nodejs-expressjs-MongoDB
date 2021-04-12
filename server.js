1ebeddabc954f9c9d80755be79705d5a5ec7a709
shivkumar patel <patelshiv7698@gmail.com>
2021-04-12 14:41:07 +0530

    Database snippets added

--- server.js
+++ server.js
@@ -3,9 +3,19 @@ const app = express();
 const path = require('path');
 const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
+//Database connection
+const url = 'mongodb://localhost/pizza';
+mongoose.connect(url, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndMondify: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database Connected...');
+}).catch(err => {
+    console.log('Connection failed....');
+});
 
 //Assets
-
 app.use(express.static('public'))
 
 //set Templets
